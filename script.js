3f9ed0edf7a0f6c3b5f6c01bb836d2468be646a2
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-11-26 14:00:58 +0600

    script

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let speech = new SpeechSynthesisUtterance();
 
+let voices = [];if
+
+let voiceSelect = document.querySelector("select");
+
+window.SpeechSynthesis.onvoiceschanged = () => {
+	voices = window.SpeechSynthesis.getVoices();
+	speech.voice = voices[0];
+
+	voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)) )
+}
